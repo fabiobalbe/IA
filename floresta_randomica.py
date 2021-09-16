33b97c9f916ce6906948253acb5ad10fbe7dcb2b
Fabio Balbe <fabiobalbe@outlook.com>
2021-09-16 19:37:44 -0300

    Floresta randomica e naive bayes

---
+++ floresta_randomica.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+
+dataset = pd.read_csv("ObesityDataSet_raw_and_data_sinthetic.csv")
+encoder = OrdinalEncoder()
+
+# codificamos as colunas que não são numéricas para valores numéricos através do encoder
+dataset["Gender"] = encoder.fit_transform(pd.DataFrame(dataset["Gender"]))
+dataset["family_history_with_overweight"] = encoder.fit_transform(
+    pd.DataFrame(dataset["family_history_with_overweight"])
+)
+dataset["FAVC"] = encoder.fit_transform(pd.DataFrame(dataset["FAVC"]))
+dataset["CAEC"] = encoder.fit_transform(pd.DataFrame(dataset["CAEC"]))
+dataset["SMOKE"] = encoder.fit_transform(pd.DataFrame(dataset["SMOKE"]))
+dataset["SCC"] = encoder.fit_transform(pd.DataFrame(dataset["SCC"]))
+dataset["CALC"] = encoder.fit_transform(pd.DataFrame(dataset["CALC"]))
+dataset["MTRANS"] = encoder.fit_transform(pd.DataFrame(dataset["MTRANS"]))
+dataset["NObeyesdad"] = encoder.fit_transform(pd.DataFrame(dataset["NObeyesdad"]))
+
+# separando os dados das classes do dataset
+dados = dataset.iloc[:, :16].values  # até a 16, não incluso a coluna 16
+nomeclasses = dataset.iloc[:, 16].values  # somente a coluna 16
+
+# construindo a floresta randomica para classificar os dados por ML
+# quebra o dataframe de teste e de treino a partir das variáveis x e y
+x_treino, x_teste, y_treino, y_teste = train_test_split(
+    dados, nomeclasses, test_size=0.25, random_state=5
+)
+# construindo floresta randomica
+florestaRandomica = RandomForestClassifier(n_estimators=1000)  # numero de arvores
+
+# treinar a floresta
+florestaRandomica.fit(x_treino, y_treino)
+
+# testar se floresta faz a predição corretamente
+resultados = florestaRandomica.predict(x_teste)
+
+print("Acurácia:", metrics.accuracy_score(y_teste, resultados))
